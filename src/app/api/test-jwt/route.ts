0306f19f38ddb590e57a7592e6e13f43bbca7c32
bao <huynhbao103@gmailgmail.com>
2025-07-29 23:42:08 +0700

    ver 4.2

--- src/app/api/test-jwt/route.ts
+++ src/app/api/test-jwt/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@/auth';
 import jwt from 'jsonwebtoken';
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
